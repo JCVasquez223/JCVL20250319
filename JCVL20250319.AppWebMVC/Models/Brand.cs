a9405fc292a56f485c4e47952bf17c454e523fb9
Vasquez <vasquez0215@outlook.com>
2025-03-19 09:01:37 -0600

    Se crea loc controladores y vistas de los modelos de marcas,proudctoss y categorias.

--- JCVL20250319.AppWebMVC/Models/Brand.cs
+++ JCVL20250319.AppWebMVC/Models/Brand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace JCVL20250319.AppWebMVC.Models;
 
@@ -7,8 +8,11 @@ public partial class Brand
 {
     public int BrandId { get; set; }
 
+
+    [Display(Name = "Marcas")]
     public string BrandName { get; set; } = null!;
 
+    [Display(Name = "Pais")]
     public string? Country { get; set; }
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
